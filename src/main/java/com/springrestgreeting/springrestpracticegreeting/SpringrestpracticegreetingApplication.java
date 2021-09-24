b09d3d04fd82b1d5d00ab0eb9163449f6dd37bc3
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-24 20:21:12 +0530

    [ruchi]: implemented hello world using rest API

---
+++ src/main/java/com/springrestgreeting/springrestpracticegreeting/SpringrestpracticegreetingApplication.java
@@ -0,0 +1,13 @@
+package com.springrestgreeting.springrestpracticegreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestpracticegreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestpracticegreetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrestgreeting/springrestpracticegreeting/SpringrestpracticegreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrestgreeting.springrestpracticegreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestpracticegreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
